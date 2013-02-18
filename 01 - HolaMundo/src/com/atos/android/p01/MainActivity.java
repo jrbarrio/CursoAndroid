15e81b8800cf77fd6257bd51647fe50441c6589b
Jorge Roldán <jorge.roldan@gmail.com>
2013-02-18 18:39:37 +0100

    introduccion de logs

--- 01 - HolaMundo/src/com/atos/android/p01/MainActivity.java
+++ 01 - HolaMundo/src/com/atos/android/p01/MainActivity.java
@@ -3,6 +3,7 @@ package com.atos.android.p01;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -25,6 +26,13 @@ public class MainActivity extends Activity implements OnClickListener {
     }
 
 	public void onClick(View v) {
+		Log.d("MainActivity", "Se ha pulsdo el boton");
+		Log.i("MainActivity", "Mensaje informativo");
+		Log.w("MainActivity", "Mensaje de aviso");
+		Log.e("MainActivity", "Mensaje de error");
+		
+		Log.println(Log.INFO, "MainActivity", "Mensaje informativo 2");
+		
 		etiqueta.setText("Me has pulsado");
 		etiqueta.setBackgroundColor(Color.GREEN);		
 	}   
