7578a8c1bbc2e8a384c581e7733a1d2b6b7fbd25
Jorge Roldán <jorge.roldan@gmail.com>
2013-02-18 18:22:32 +0100

    empezando con los eventos

--- 01 - HolaMundo/src/com/atos/android/p01/MainActivity.java
+++ 01 - HolaMundo/src/com/atos/android/p01/MainActivity.java
@@ -1,24 +1,36 @@
 package com.atos.android.p01;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private TextView etiqueta;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
     
+        etiqueta = (TextView) findViewById(R.id.etiqueta);
+        etiqueta.setText("Hola Mundo!");
+        
+        Button boton = (Button)findViewById(R.id.boton);
+        boton.setOnClickListener(this);
+    }
+
+	public void onClick(View v) {
+		etiqueta.setText("Me has pulsado");
+		etiqueta.setBackgroundColor(Color.GREEN);		
+	}   
+	
+	public void botonPulsado(View v) {
+		etiqueta.setText("Has pulsado otro boton");
+		etiqueta.setBackgroundColor(Color.MAGENTA);	
+	}
 }
