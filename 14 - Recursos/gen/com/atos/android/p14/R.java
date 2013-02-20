801571e9d4a3254c7305c4706a7c084900c064c6
Jorge Roldán <jorge.roldan@gmail.com>
2013-02-20 18:56:46 +0100

    mas ejemplos de manejos de recursos

--- 14 - Recursos/gen/com/atos/android/p14/R.java
+++ 14 - Recursos/gen/com/atos/android/p14/R.java
@@ -8,6 +8,9 @@
 package com.atos.android.p14;
 
 public final class R {
+    public static final class array {
+        public static final int estaciones=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,28 +20,34 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int etiqueta=0x7f080000;
-        public static final int orientacion=0x7f080001;
+        public static final int action_settings=0x7f0a0003;
+        public static final int etiqueta=0x7f0a0000;
+        public static final int orientacion=0x7f0a0001;
+        public static final int spinner1=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int android=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040003;
-        public static final int app_name=0x7f040002;
-        public static final int hello_world=0x7f040001;
-        public static final int orientacion=0x7f040000;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050002;
+        public static final int frase=0x7f050005;
+        public static final int hello_world=0x7f050001;
+        public static final int orientacion=0x7f050000;
+        public static final int title_activity_main=0x7f050004;
     }
     public static final class style {
         /** 
@@ -51,10 +60,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- 14 - Recursos/src/com/atos/android/p14/MainActivity.java
+++ 14 - Recursos/src/com/atos/android/p14/MainActivity.java
@@ -1,7 +1,13 @@
 package com.atos.android.p14;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.Resources.NotFoundException;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +16,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Resources res = getResources();
+		String cadena = res.getString(R.string.frase, "pepe", 1500);
+		Log.i("MAinActivity", "Cadena leida = " + cadena);
+		
+		String[] valores = res.getStringArray(R.array.estaciones);
+		
+		try {
+			InputStream in = res.openRawResource(R.raw.android);
+			in.close();
+		} catch (NotFoundException e) {
+			Log.e("MAinActivity", "Error. Recurso no encontrado.");
+		} catch (IOException e) {
+			Log.e("MAinActivity", "Error de E/S.");
+		}
 	}
 
 }
