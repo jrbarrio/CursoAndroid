0aa8357930bcb1ef49eefade5655ea4132a39076
Jorge Roldán <jorge.roldan@gmail.com>
2013-02-21 19:10:25 +0100

    manejo de imagenes

--- 17 - Graficos/src/com/atos/android/p17/Lienzo.java
+++ 17 - Graficos/src/com/atos/android/p17/Lienzo.java
@@ -1,13 +1,19 @@
 package com.atos.android.p17;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class Lienzo extends View {
@@ -16,17 +22,14 @@ public class Lienzo extends View {
 
 	public Lienzo(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Lienzo(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Lienzo(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 	} 
 	
 	@Override
@@ -48,10 +51,25 @@ public class Lienzo extends View {
 //		canvas.drawText("Hola Mundo", 0, 120, brocha);
 		
 		AssetManager assets = getContext().getAssets();
-		Typeface fuente = Typeface.createFromAsset(assets, "fuentes/E111VIVA.TTF");
-		brocha.setTypeface(fuente);
-		brocha.setTextSize(40);
-		canvas.drawText("Hola Mundo", 0, 100, brocha);
+//		Typeface fuente = Typeface.createFromAsset(assets, "fuentes/E111VIVA.TTF");
+//		brocha.setTypeface(fuente);
+//		brocha.setTextSize(40);
+//		canvas.drawText("Hola Mundo", 0, 100, brocha);
+		
+		try {
+			InputStream is = assets.open("images/android.png");
+			Bitmap bmp = BitmapFactory.decodeStream(is);
+			
+			canvas.scale(0.5f, 0.5f);
+			canvas.drawBitmap(bmp, 0, 0, brocha);
+			canvas.drawBitmap(bmp, 50, 50, brocha);
+			canvas.drawBitmap(bmp, 
+					new Rect(0, 0, 60, 60),
+					new RectF(0, 0, 30, 60),
+					brocha);
+		} catch (IOException e) {
+			Log.e("Lienzo", "Error de lectura de android.png", e);
+		}
 	}
 	
 	public int getColorCirculo() {
